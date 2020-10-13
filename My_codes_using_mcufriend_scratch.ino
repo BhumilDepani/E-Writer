393ac2f76364f8e61a32866bb8da0badfb74a23b
BhumilDepani <bhumil.depani1@gmail.com>
2020-10-13 09:18:39 +0530

    Final submission of codes

---
+++ My_codes_using_mcufriend_scratch.ino
@@ -0,0 +1,190 @@
+ #include "MCUFRIEND_kbv.h"
+MCUFRIEND_kbv tft;       // hard-wired for UNO shields anyway.
+#include "touchScreen.h"
+
+char *name = "Welcome To FLASHBOOK";  //edit name of shield
+const int XP=6,YM=7; //ID=0x9341
+const int TS_LEFT=907,TS_RT=136,TS_TOP=942,TS_BOT=139;
+
+#define XM A2
+#define YP A1
+
+TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
+TSPoint tp;
+
+#define MINPRESSURE 200
+#define MAXPRESSURE 1000
+
+int16_t BOXSIZE;
+int16_t PENRADIUS = 2;
+uint16_t ID, oldcolor, currentcolor;
+uint8_t Orientation = 0;    //PORTRAIT
+
+// Assign human-readable names to some common 16-bit color values:
+#define BLACK   0x0000
+#define BLUE    0x001F
+#define RED     0xF800
+#define GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+#define ORANGE  0xe940
+
+void show_tft(void)
+{
+    tft.setCursor(0,0);
+    tft.setTextSize(1);
+    tft.print(F("ID=0x"));
+    tft.println(ID, HEX);
+    tft.println("Screen is " + String(tft.width()) + "x" + String(tft.height()));
+    tft.println("");
+    tft.setTextSize(2);
+    tft.println(name);
+    tft.setTextSize(1);
+    tft.println("PORTRAIT Values:");
+    tft.println("LEFT = " + String(TS_LEFT) + " RT  = " + String(TS_RT));
+    tft.println("TOP  = " + String(TS_TOP)  + " BOT = " + String(TS_BOT));
+    tft.println("\nWiring is: ");
+    tft.println("YP=" + String(YP)  + " XM=" + String(XM));
+    tft.println("YM=" + String(YM)  + " XP=" + String(XP));
+    tft.setTextSize(2);
+    tft.setTextColor(RED);
+    tft.setCursor((tft.width() - 48) / 2, (tft.height() ) / 2);
+    tft.print("EXIT");
+    tft.setTextColor(YELLOW, BLACK);
+    tft.setCursor(0, (tft.height() * 6) / 8);
+    tft.print("Touch screen for loc");
+    while (1) {
+        tp = ts.getPoint();
+        pinMode(XM, OUTPUT);
+        pinMode(YP, OUTPUT);
+        if (tp.z < MINPRESSURE || tp.z > MAXPRESSURE) continue;
+        if (tp.x > 450 && tp.x < 570  && tp.y > 450 && tp.y < 570) break;
+        tft.setCursor(0, (tft.height() * 3) / 4);
+        tft.print("tp.x=" + String(tp.x) + " tp.y=" + String(tp.y) + "   ");
+    }
+}
+
+
+void setup(void)
+{
+    uint16_t tmp;
+
+    tft.reset();
+    ID = tft.readID();
+    tft.begin(ID);
+    //printf("lol and ID is %ud",ID);
+    Serial.begin(9600);
+    tft.setRotation(Orientation);
+    tft.fillScreen(BLACK);
+    show_tft();
+
+    BOXSIZE = tft.width() / 6;
+    tft.fillScreen(WHITE);
+
+    tft.fillRect(0, 279, BOXSIZE, BOXSIZE, RED);
+    tft.fillRect(BOXSIZE, 279, BOXSIZE, BOXSIZE, BLACK);
+    tft.fillRect(BOXSIZE * 2, 279, BOXSIZE, BOXSIZE, GREEN);
+    tft.fillRect(BOXSIZE * 3, 279, BOXSIZE, BOXSIZE, ORANGE);
+    tft.fillRect(BOXSIZE * 4, 279, BOXSIZE, BOXSIZE, BLUE);
+    tft.fillRect(BOXSIZE * 5, 279, BOXSIZE, BOXSIZE, WHITE);
+
+    tft.drawRect(0, 0, BOXSIZE, BOXSIZE, WHITE);
+    currentcolor = RED;
+    tft.drawLine(0,28,239,28,RED);
+    tft.drawLine(0,30,239,30,RED);
+    tft.drawLine(28,0,28,279,RED);
+    tft.drawLine(30,0,30,279,RED);
+    
+    for(int i=35;i<279;i=i+10)
+    {                                                           
+    tft.drawLine(0,i,239,i,BLACK);
+    }
+    delay(1000);
+    
+}
+
+void loop()
+{
+    uint16_t xpos, ypos;  //screen coordinates
+    tp = ts.getPoint();   //tp.x, tp.y are ADC values
+
+    // if sharing pins, you'll need to fix the directions of the touchscreen pins
+    pinMode(XM, OUTPUT);
+    pinMode(YP, OUTPUT);
+    // we have some minimum pressure we consider 'valid'
+    // pressure of 0 means no pressing!
+
+    if (tp.z > MINPRESSURE && tp.z < MAXPRESSURE) {
+        // most mcufriend have touch (with icons) that extends below the TFT
+        // screens without icons need to reserve a space for "erase"
+        // scale the ADC values from ts.getPoint() to screen values e.g. 0-239
+        //
+        // Calibration is true for PORTRAIT. tp.y is always long dimension 
+        // map to your current pixel orientation
+        switch (Orientation) {
+            case 0:
+                xpos = map(tp.x, TS_LEFT, TS_RT, 0, tft.width());
+                ypos = map(tp.y, TS_TOP, TS_BOT, 0, tft.height());
+                break;
+            case 1:
+                xpos = map(tp.y, TS_TOP, TS_BOT, 0, tft.width());
+                ypos = map(tp.x, TS_RT, TS_LEFT, 0, tft.height());
+                break;
+            case 2:
+                xpos = map(tp.x, TS_RT, TS_LEFT, 0, tft.width());
+                ypos = map(tp.y, TS_BOT, TS_TOP, 0, tft.height());
+                break;
+            case 3:
+                xpos = map(tp.y, TS_BOT, TS_TOP, 0, tft.width());
+                ypos = map(tp.y, TS_LEFT, TS_RT, 0, tft.height());
+                break;
+        }
+
+        // are we in top color box area ?
+        if (ypos < BOXSIZE) {               //draw white border on selected color box
+            oldcolor = currentcolor;
+
+            if (xpos < BOXSIZE) {
+                currentcolor = RED;
+                tft.drawRect(0, 0, BOXSIZE, BOXSIZE, WHITE);
+            } else if (xpos < BOXSIZE * 2) {
+                currentcolor = BLACK;
+                tft.drawRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, WHITE);
+            } else if (xpos < BOXSIZE * 3) {
+                currentcolor = GREEN;
+                tft.drawRect(BOXSIZE * 2, 0, BOXSIZE, BOXSIZE, WHITE);
+            } else if (xpos < BOXSIZE * 4) {
+                currentcolor = ORANGE;
+                tft.drawRect(BOXSIZE * 3, 0, BOXSIZE, BOXSIZE, WHITE);
+            } else if (xpos < BOXSIZE * 5) {
+                currentcolor = BLUE;
+                tft.drawRect(BOXSIZE * 4, 0, BOXSIZE, BOXSIZE, WHITE);
+            } else if (xpos < BOXSIZE * 6) {
+                currentcolor = WHITE;
+                tft.drawRect(BOXSIZE * 5, 0, BOXSIZE, BOXSIZE, WHITE);
+            }
+
+            if (oldcolor != currentcolor) { //rub out the previous white border
+                if (oldcolor == RED) tft.fillRect(0, 0, BOXSIZE, BOXSIZE, RED);
+                if (oldcolor == YELLOW) tft.fillRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, YELLOW);
+                if (oldcolor == GREEN) tft.fillRect(BOXSIZE * 2, 0, BOXSIZE, BOXSIZE, GREEN);
+                if (oldcolor == CYAN) tft.fillRect(BOXSIZE * 3, 0, BOXSIZE, BOXSIZE, CYAN);
+                if (oldcolor == BLUE) tft.fillRect(BOXSIZE * 4, 0, BOXSIZE, BOXSIZE, BLUE);
+                if (oldcolor == MAGENTA) tft.fillRect(BOXSIZE * 5, 0, BOXSIZE, BOXSIZE, MAGENTA);
+            }
+        }
+        // are we in drawing area ?
+        if (((ypos - PENRADIUS) > BOXSIZE) && ((ypos + PENRADIUS) < tft.height())) {
+            tft.fillCircle(xpos, ypos, PENRADIUS, currentcolor);
+        }
+        // are we in erase area ?
+        // Plain Touch panels use bottom 10 pixels e.g. > h - 10
+        // Touch panels with icon area e.g. > h - 0
+        if (ypos > tft.height() - 10) {
+            // press the bottom of the screen to erase
+            tft.fillRect(0, BOXSIZE, tft.width(), tft.height() - BOXSIZE, BLACK);
+        }
+    }
+}
